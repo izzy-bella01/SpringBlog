b3228e3cf4171bb579de6be6df486425b37bc3b2
Isabella <isabellarose.hernandez01@gmail.com>
2022-04-19 11:32:21 -0500

    finished dice roll game

---
+++ src/main/java/com/codeup/springblog/DiceRollController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Random;
+
+@Controller
+public class DiceRollController {
+
+    @GetMapping("/roll-dice")
+    public String diceRoll() {
+        return "diceRoll";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String diceRollGuess(@PathVariable int n, Model model) {
+        model.addAttribute("guessedNum", n);
+
+        int number = (int) (Math.floor(Math.random() * ((6 - 1) + 1)) + 1);
+        model.addAttribute("randomNum", number);
+
+        return "diceRollGuess";
+    }
+//
+//    public double getRandomNumber() {
+//        double x = (Math.random() * ((6 - 1) + 1)) + 1;
+//        return x;
+//    }
+}
